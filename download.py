31a4dde588ce2f3a113eda44aff0ba6a417fc9db
Phaedon Valasiadis <faival@protonmail.com>
2022-11-16 11:24:48 +0000

    added paste-url button but get_clipboard needs fixes

--- download.py
+++ download.py
@@ -19,6 +19,9 @@ def select_path():
     path = filedialog.askdirectory()
     path_label.config(text=path)
 
+def paste_url():
+    link_field.insert(0, cliptext)
+
 def download_file():
     #get usr link
     get_link = link_field.get() 
@@ -34,6 +37,12 @@ def download_file():
     shutil.move(mp4_video, user_path)
     screen.title("Dead Pulled!!! Pull Another One...")
 
+def do_popup(event):
+    try:
+        m.tk_popup(event.x_screen, event.y_screen)
+    finally:
+        m.grab_release()
+
 screen = Tk()
 title = screen.title("Dead Pull")
 canvas = Canvas(screen, width=500, height=500)
@@ -44,9 +53,17 @@ logo_img = PhotoImage(file="cute_d.png")
 logo_img = logo_img.subsample(4, 4)
 canvas.create_image(250, 80, image=logo_img)
 
+#add temporary directional text into txt-field
+def temp_text(e):
+   link_field.delete(0,"end")
+
 #link field
 link_field = Entry(screen, width=50)
 link_label = Label(screen, text="Enter Video Url: ", font=('Roboto', 18))
+#temporary
+link_field.insert(0, "Please press Ctrl V, to paste video url")
+link_field.pack(pady=20)
+link_field.bind("<FocusIn>", temp_text)
 
 #select save location
 path_label = Label(screen, text="Select Pull Location", font=('Roboto', 15))
@@ -60,6 +77,14 @@ canvas.create_window(250, 330, window=select_btn)
 canvas.create_window(250, 190, window=link_label)
 canvas.create_window(250, 220, window=link_field)
 
+# Get the data from the clipboard
+cliptext = screen.clipboard_get()
+
+#paste button
+paste_btn = Button(screen, text="Paste Url", command=paste_url)
+
+canvas.create_window(90, 250, window=paste_btn)
+
 #download buttons
 download_btn = Button(screen, text="Pull It", command=download_file)
 
@@ -67,6 +92,6 @@ download_btn = Button(screen, text="Pull It", command=download_file)
 canvas.create_window(250, 390, window=download_btn)
 
 
-
+###############################################################
 
 screen.mainloop()
