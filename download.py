39beb844a0f5bfd9cc58344510ef27a5841bf537
Phaedon Valasiadis <faival@protonmail.com>
2022-11-16 01:19:26 +0000

    main window

---
+++ download.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter import filedialog
+
+screen = Tk()
+title = screen.title("Dead Pull")
+canvas = Canvas(screen, width=500, height=500)
+canvas.pack()
+
+screen.mainloop()
