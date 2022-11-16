3dd9d1a4be902b25d4d4d06483163ad571d4f4b7
Phaedon Valasiadis <faival@protonmail.com>
2022-11-16 02:10:48 +0000

    imports

--- download.py
+++ download.py
@@ -1,5 +1,18 @@
+# You need to install moviepy && pytube
+# pip install moviepy
+# pip install pytube
+
+from os import path
 from tkinter import *
 from tkinter import filedialog
+from moviepy import *
+from moviepy.editor import VideoFileClip
+from pytube import YouTube
+
+#functions
+def select_path():
+    path = filedialog.askdirectory()
+    path_label.config(text=path)
 
 screen = Tk()
 title = screen.title("Dead Pull")
@@ -17,7 +30,7 @@ link_label = Label(screen, text="Enter Video Url: ", font=('Roboto', 18))
 
 #select save location
 path_label = Label(screen, text="Select Pull Location", font=('Roboto', 15))
-select_btn = Button(screen, text="Select")
+select_btn = Button(screen, text="Select", command=select_path)
 
 #add to window
 canvas.create_window(250, 280, window=path_label)
